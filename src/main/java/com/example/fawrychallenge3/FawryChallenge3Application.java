f7141291566dce9f0b0bab3ed9e5918f45279697
oshafcai <oshasaad968@gmail.com>
2025-07-04 21:34:09 +0300

    Initial commit

---
+++ src/main/java/com/example/fawrychallenge3/FawryChallenge3Application.java
@@ -0,0 +1,13 @@
+package com.example.fawrychallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawryChallenge3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FawryChallenge3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fawrychallenge3/FawryChallenge3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fawrychallenge3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawryChallenge3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
